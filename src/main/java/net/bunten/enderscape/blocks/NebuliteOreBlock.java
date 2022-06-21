35e1491d003a78cc56049f9d9961693aecf6f841
buntennn <bunten2005@gmail.com>
2022-06-21 03:48:48 -0500

    nebulite ore refactor

--- src/main/java/net/bunten/enderscape/blocks/NebuliteOreBlock.java
+++ src/main/java/net/bunten/enderscape/blocks/NebuliteOreBlock.java
@@ -31,52 +31,46 @@ public class NebuliteOreBlock extends OreBlock {
             var pos2 = pos.offset(dir);
             if (world.getBlockState(pos2).isOpaque()) {
                 i++;
+
+                if (i == 6) return true;
             }
         }
-        return i == 6;
+        return false;
     }
 
     @Environment(EnvType.CLIENT)
-    protected void tryPlayAmbientSound(BlockState state, World world, BlockPos pos, Random random) {
-        final var client = MinecraftClient.getInstance();
-        var pos2 = client.cameraEntity.getBlockPos();
-        var distance = pos.getSquaredDistance(pos2);
-
-        SoundEvent sound;
-
-        if (isBlockObstructed(world, pos)) {
-            sound = EnderscapeSounds.BLOCK_NEBULITE_ORE_AMBIENT_OBSTRUCTED;
+    protected SoundEvent getAmbientSound(World world, BlockPos cameraPos, BlockPos nebulitePos) {
+        if (isBlockObstructed(world, nebulitePos)) {
+            return EnderscapeSounds.BLOCK_NEBULITE_ORE_AMBIENT_OBSTRUCTED;
         } else {
-            if (distance > 70) {
-                sound = EnderscapeSounds.BLOCK_NEBULITE_ORE_AMBIENT_FAR;
+            if (nebulitePos.isWithinDistance(cameraPos, 16)) {
+                return EnderscapeSounds.BLOCK_NEBULITE_ORE_AMBIENT;
             } else {
-                sound = EnderscapeSounds.BLOCK_NEBULITE_ORE_AMBIENT;
+                return EnderscapeSounds.BLOCK_NEBULITE_ORE_AMBIENT_FAR;
             }
         }
+    }
 
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
+    @Environment(EnvType.CLIENT)
+    protected void tryPlayAmbientSound(BlockState state, World world, BlockPos nebulitePos, Random random) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        BlockPos cameraPos = client.cameraEntity.getBlockPos();
+        
+        float volume = MathUtil.nextFloat(random, 0.9F, 1.1F);
+        float pitch = MathUtil.nextFloat(random, 0.9F, 1.1F);
 
-        world.playSound(x, y, z, sound, SoundCategory.BLOCKS, MathUtil.nextFloat(random, 0.8F, 1), MathUtil.nextFloat(random, 0.9F, 1.1F), false);
+        world.playSound(cameraPos.getX(), cameraPos.getY(), cameraPos.getZ(), getAmbientSound(world, cameraPos, nebulitePos), SoundCategory.BLOCKS, volume, pitch, false);
     }
 
     @Environment(EnvType.CLIENT)
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
         for (int i = 0; i < 5; i++) {
-            int x = pos.getX();
-            int y = pos.getY();
-            int z = pos.getZ();
-
-            double vx = MathUtil.nextFloat(random, -0.01F, 0.01F);
-            double vy = MathUtil.nextFloat(random, -0.01F, 0.01F);
-            double vz = MathUtil.nextFloat(random, -0.01F, 0.01F);
-
             Mutable mutable = new Mutable();
-            mutable.set(x + MathUtil.nextInt(random, -5, 5), y + MathUtil.nextInt(random, -5, 5), z + MathUtil.nextInt(random, -5, 5));
+            mutable.set(pos.getX() + MathUtil.nextInt(random, -5, 5), pos.getY() + MathUtil.nextInt(random, -5, 5), pos.getZ()+ MathUtil.nextInt(random, -5, 5));
             
             if (!world.getBlockState(mutable).isFullCube(world, mutable)) {
-                world.addParticle(new DustParticleEffect(new Vec3f(Vec3d.unpackRgb(0xB328D2)), 1), mutable.getX() + random.nextDouble(), mutable.getY() + random.nextDouble(), mutable.getZ() + random.nextDouble(), vx, vy, vz);
+                var color = new Vec3f(Vec3d.unpackRgb(0xB328D2));
+                world.addParticle(new DustParticleEffect(color, 1), mutable.getX() + random.nextDouble(), mutable.getY() + random.nextDouble(), mutable.getZ() + random.nextDouble(), 0, 0, 0);
             }
         }
 
