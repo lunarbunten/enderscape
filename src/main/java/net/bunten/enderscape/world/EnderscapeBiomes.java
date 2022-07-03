37603d69ce5e435f45dbbe223af23f3c71ea3feb
buntennn <bunten2005@gmail.com>
2022-07-03 15:01:59 -0500

    improve biome stuff
    remove highlands overwriting
    fix biome modifications
    add celestial plains to is_end tag

--- src/main/java/net/bunten/enderscape/world/EnderscapeBiomes.java
+++ src/main/java/net/bunten/enderscape/world/EnderscapeBiomes.java
@@ -7,19 +7,18 @@ import net.bunten.enderscape.Enderscape;
 import net.bunten.enderscape.registry.EnderscapeEntities;
 import net.bunten.enderscape.world.biomes.CelestialIslandsBiome;
 import net.bunten.enderscape.world.biomes.CelestialPlainsBiome;
-import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.EndPlacedFeatures;
 
 public class EnderscapeBiomes {
-
-    public static final BCLBiome END_HIGHLANDS = new BCLBiome(BiomeKeys.END_HIGHLANDS.getValue(), BuiltinRegistries.BIOME.get(BiomeKeys.END_HIGHLANDS.getValue()));
 
     public static final BCLBiome CELESTIAL_PLAINS = CelestialPlainsBiome.register();
     public static final BCLBiome CELESTIAL_ISLANDS = CelestialIslandsBiome.register();
 
-    private static void addGlobalFeatures(RegistryEntry<Biome> biome) {
+    private static void addGlobalModifications(RegistryEntry<Biome> biome) {
         BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_SHADOW_QUARTZ_ORE);
         BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_SCATTERED_SHADOW_QUARTZ_ORE);
 
@@ -27,39 +26,46 @@ public class EnderscapeBiomes {
         BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_VOID_NEBULITE_ORE);
     }
 
-    private static void addEndHighlandsFeatures(RegistryEntry<Biome> biome) {
-        BiomeAPI.addBiomeMobSpawn(biome, EnderscapeEntities.RUBBLEMITE, 2, 2, 4);
-
-        BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_UNCOMMON_CELESTIAL_GROWTH);
-        BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_MURUSHROOMS);
-    }
-
-    private static void addBetterEndAccommodations(RegistryEntry<Biome> biome) {
+    private static void addBetterEndModifications(RegistryEntry<Biome> biome) {
         BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BETTEREND_SHADOW_QUARTZ_ORE);
         BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BETTEREND_VOID_NEBULITE_ORE);
         BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BETTEREND_MURUSHROOMS);
     }
 
-    public static void init() {
-        BiomeAPI.registerEndLandBiome(END_HIGHLANDS);
+    private static void addDefaultEndModifications(RegistryEntry<Biome> biome, boolean midlands) {
+        BiomeAPI.addBiomeMobSpawn(biome, EnderscapeEntities.RUBBLEMITE, 2, 2, 4);
 
-        BiomeAPI.registerEndLandBiome(CELESTIAL_PLAINS);
-        BiomeAPI.registerEndVoidBiome(CELESTIAL_ISLANDS);
+        BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_UNCOMMON_CELESTIAL_GROWTH);
+        BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_MURUSHROOMS);
 
+        if (midlands) BiomeAPI.addBiomeFeature(biome, GenerationStep.Feature.VEGETAL_DECORATION, EndPlacedFeatures.CHORUS_PLANT);
+    }
+
+    private static void registerBiomeModifications() {
         BiomeAPI.registerEndBiomeModification((id, biome) -> {
-			if (id.getNamespace() != "enderscape" && id != BiomeKeys.THE_END.getValue()) {
-                addGlobalFeatures(biome);
+            if (id == BiomeKeys.THE_END.getValue()) return;
+
+			if (id.getNamespace() != "enderscape") {
+                addGlobalModifications(biome);
+
+                boolean highlands = id == BiomeKeys.END_HIGHLANDS.getValue();
+                boolean midlands = id == BiomeKeys.END_MIDLANDS.getValue();
                 
-                if (id == BiomeKeys.END_HIGHLANDS.getValue()) {
-                    addEndHighlandsFeatures(biome);
+                if (highlands || midlands) {
+                    addDefaultEndModifications(biome, midlands);
                 } else {
                     BiomeAPI.addBiomeFeature(biome, EnderscapeFeatures.BCL_UNCOMMON_MURUSHROOMS);
                 }
 			}
 
-            if (Enderscape.hasBetterEnd()) {
-                addBetterEndAccommodations(biome);
-            }
+            if (Enderscape.hasBetterEnd()) addBetterEndModifications(biome);
 		});
+    }
+
+    public static void init() {
+        BiomeAPI.registerEndLandBiome(CELESTIAL_PLAINS);
+        BiomeAPI.registerEndVoidBiome(CELESTIAL_ISLANDS);
+
+        registerBiomeModifications();
     }
 }
