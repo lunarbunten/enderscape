3669058a14fa88be16931ff77ff0355a86aebe85
bunten <bunten2005@gmail.com>
2025-05-30 14:40:37 -0500

    Fix SIHYWTCAMD crash

--- src/main/java/net/bunten/enderscape/mixin/ShulkerBulletMixin.java
+++ src/main/java/net/bunten/enderscape/mixin/ShulkerBulletMixin.java
@@ -1,12 +1,10 @@
 package net.bunten.enderscape.mixin;
 
-import com.google.common.base.MoreObjects;
 import net.bunten.enderscape.EnderscapeConfig;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.entity.projectile.ShulkerBullet;
 import net.minecraft.world.level.Level;
@@ -15,13 +13,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ShulkerBullet.class)
 public abstract class ShulkerBulletMixin extends Projectile {
-
-    private final ShulkerBullet bullet = (ShulkerBullet) (Object) this;
 
     @Shadow protected abstract void destroy();
 
@@ -43,9 +39,11 @@ public abstract class ShulkerBulletMixin extends Projectile {
         }
     }
 
-    @Redirect(method = "onHitEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z"))
-    protected boolean onHitEntity(LivingEntity instance, MobEffectInstance mobEffectInstance, Entity entity) {
-        if (EnderscapeConfig.getInstance().shulkerBulletRebalanceLevitation) return instance.addEffect(new MobEffectInstance(MobEffects.LEVITATION, 20 * 4, 2), MoreObjects.firstNonNull(entity, bullet));
-        return instance.addEffect(mobEffectInstance, MoreObjects.firstNonNull(entity, bullet));
+    @ModifyArg(method = "onHitEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;Lnet/minecraft/world/entity/Entity;)Z"))
+    protected MobEffectInstance onHitEntity(MobEffectInstance original) {
+        if (EnderscapeConfig.getInstance().shulkerBulletRebalanceLevitation) {
+            return new MobEffectInstance(MobEffects.LEVITATION, 20 * 4, 2);
+        }
+        return original;
     }
 }
