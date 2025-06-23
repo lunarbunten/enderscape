089d7fb80399d197c25fe5e3d865b33c6e0dc911
bunten <bunten2005@gmail.com>
2025-06-22 22:40:48 -0500

    revert fog color change and bump version

--- src/main/java/net/bunten/enderscape/client/mixin/EndEffectsMixin.java
+++ src/main/java/net/bunten/enderscape/client/mixin/EndEffectsMixin.java
@@ -1,6 +1,5 @@
 package net.bunten.enderscape.client.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.bunten.enderscape.EnderscapeConfig;
 import net.bunten.enderscape.client.world.EnderscapeSkybox;
 import net.fabricmc.api.EnvType;
@@ -9,14 +8,17 @@ import net.minecraft.client.renderer.DimensionSpecialEffects;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(DimensionSpecialEffects.EndEffects.class)
 public abstract class EndEffectsMixin {
 
-    @ModifyReturnValue(method = "getBrightnessDependentFogColor", at = @At("RETURN"))
-    public Vec3 Enderscape$getBrightnessDependentFogColor(Vec3 original) {
-        if (EnderscapeConfig.getInstance().skyboxUpdateEnabled) return original.scale(EnderscapeSkybox.gammaFactor());
-        return original;
+    @Inject(method = "getBrightnessDependentFogColor", at = @At("HEAD"), cancellable = true)
+    public void Enderscape$getBrightnessDependentFogColor(Vec3 vec3, float f, CallbackInfoReturnable<Vec3> info) {
+        if (EnderscapeConfig.getInstance().skyboxUpdateEnabled) {
+            info.setReturnValue(vec3.scale(EnderscapeSkybox.gammaFactor()));
+        }
     }
 }
