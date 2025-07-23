7ee48f42421784b414df0d3d31be1332d4ea9917
bunten <bunten2005@gmail.com>
2025-07-23 10:02:30 -0500

    update situational music code

--- src/main/java/net/bunten/enderscape/client/mixin/MinecraftMixin.java
+++ src/main/java/net/bunten/enderscape/client/mixin/MinecraftMixin.java
@@ -3,9 +3,10 @@ package net.bunten.enderscape.client.mixin;
 import net.bunten.enderscape.client.EnderscapeClient;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.Optionull;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.WinScreen;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.sounds.Music;
 import net.minecraft.sounds.Musics;
@@ -28,19 +29,24 @@ public abstract class MinecraftMixin {
 
     @Inject(method = "getSituationalMusic", at = @At("HEAD"), cancellable = true)
     public void getSituationalMusic(CallbackInfoReturnable<Music> info) {
-        if (instance.screen instanceof WinScreen && !instance.getMusicManager().isPlayingMusic(Musics.CREDITS)) instance.getSoundManager().stop();
+        Music music = Optionull.map(instance.screen, Screen::getBackgroundMusic);
+
+        if (music != null) {
+            if (instance.screen instanceof WinScreen && !instance.getMusicManager().isPlayingMusic(Musics.CREDITS)) instance.getSoundManager().stop();
+            return;
+        }
 
         LocalPlayer player = instance.player;
-        ClientLevel level = instance.level;
 
-        if (player != null && level.dimension() == Level.END) {
-            Biome biome = level.getBiome(player.blockPosition()).value();
-            Optional<Music> optional = EnderscapeClient.structureMusic.isPresent() ? EnderscapeClient.structureMusic : biome.getBackgroundMusic();
-
-            if (optional.isPresent()) {
-                Music music = optional.get();
-                info.setReturnValue(music);
-            }
+        if (player != null && !instance.gui.getBossOverlay().shouldPlayMusic()) {
+            EnderscapeClient.structureMusic.ifPresentOrElse(info::setReturnValue, () -> {
+                Level level = player.level();
+                if (level.dimension() == Level.END) {
+                    Biome biome = level.getBiome(player.blockPosition()).value();
+                    Optional<Music> optional = biome.getBackgroundMusic();
+                    optional.ifPresent(info::setReturnValue);
+                }
+            });
         }
     }
 }
