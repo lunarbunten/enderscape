07d84b7270a47350f3754b9d6d44d675630024df
Voidsong Dragonfly <voidsongdragonfly@pm.me>
2025-08-20 00:26:50 -0400

    Fix #95 by removing fruit tag from flanger berries

--- src/main/java/net/bunten/enderscape/datagen/EnderscapeItemTagProvider.java
+++ src/main/java/net/bunten/enderscape/datagen/EnderscapeItemTagProvider.java
@@ -100,7 +100,6 @@ public class EnderscapeItemTagProvider extends FabricTagProvider<Item> {
         getOrCreateTagBuilder(BUCKETS).add(RUSTLE_BUCKET);
         getOrCreateTagBuilder(EDIBLE_WHEN_PLACED_FOODS).add(CHORUS_CAKE_ROLL_ITEM);
         getOrCreateTagBuilder(FOODS).add(DRIFT_JELLY_BOTTLE);
-        getOrCreateTagBuilder(FRUIT_FOODS).add(FLANGER_BERRY);
         getOrCreateTagBuilder(BERRY_FOODS).add(FLANGER_BERRY);
         getOrCreateTagBuilder(GEMS).add(NEBULITE);
         getOrCreateTagBuilder(INGOTS).add(SHADOLINE_INGOT);
