ff91a2259de05a6113f2bd1e9f8d9a73013805ea
bunten <bunten2005@gmail.com>
2025-09-30 16:19:45 -0500

    add missing hyphae and wood recipes

--- src/main/java/net/bunten/enderscape/datagen/EnderscapeRecipeProvider.java
+++ src/main/java/net/bunten/enderscape/datagen/EnderscapeRecipeProvider.java
@@ -386,6 +386,9 @@ public class EnderscapeRecipeProvider extends FabricRecipeProvider {
                         .unlockedBy("has_wisp_flower", has(WISP_FLOWER))
                         .save(output, "enderscape:white_dye_from_wisp_flower");
 
+                woodFromLogs(VEILED_WOOD, VEILED_LOG);
+                woodFromLogs(STRIPPED_VEILED_WOOD, STRIPPED_VEILED_LOG);
+
                 shapeless(RecipeCategory.BUILDING_BLOCKS, VEILED_PLANKS, 4)
                         .requires(EnderscapeItemTags.VEILED_LOGS)
                         .unlockedBy("has_veiled_logs", has(EnderscapeItemTags.VEILED_LOGS))
@@ -426,6 +429,9 @@ public class EnderscapeRecipeProvider extends FabricRecipeProvider {
                 stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, CELESTIAL_BRICK_STAIRS, CELESTIAL_BRICKS);
                 stonecutterResultFromBase(RecipeCategory.DECORATIONS, CELESTIAL_BRICK_WALL, CELESTIAL_BRICKS);
 
+                woodFromLogs(CELESTIAL_HYPHAE, CELESTIAL_STEM);
+                woodFromLogs(STRIPPED_CELESTIAL_HYPHAE, STRIPPED_CELESTIAL_STEM);
+
                 shapeless(RecipeCategory.BUILDING_BLOCKS, CELESTIAL_PLANKS, 4)
                         .requires(EnderscapeItemTags.CELESTIAL_STEMS)
                         .unlockedBy("has_celestial_stems", has(EnderscapeItemTags.CELESTIAL_STEMS))
@@ -459,6 +465,9 @@ public class EnderscapeRecipeProvider extends FabricRecipeProvider {
                 stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, MURUBLIGHT_BRICK_SLAB, MURUBLIGHT_BRICKS, 2);
                 stonecutterResultFromBase(RecipeCategory.BUILDING_BLOCKS, MURUBLIGHT_BRICK_STAIRS, MURUBLIGHT_BRICKS);
                 stonecutterResultFromBase(RecipeCategory.DECORATIONS, MURUBLIGHT_BRICK_WALL, MURUBLIGHT_BRICKS);
+
+                woodFromLogs(MURUBLIGHT_HYPHAE, MURUBLIGHT_STEM);
+                woodFromLogs(STRIPPED_MURUBLIGHT_HYPHAE, STRIPPED_MURUBLIGHT_STEM);
 
                 shapeless(RecipeCategory.BUILDING_BLOCKS, MURUBLIGHT_PLANKS, 4)
                         .requires(EnderscapeItemTags.MURUBLIGHT_STEMS)
