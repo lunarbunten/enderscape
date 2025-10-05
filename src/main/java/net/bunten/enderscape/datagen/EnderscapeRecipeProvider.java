3b61a62f7ca140151e4040e174adc3d0ede4f955
bunten <bunten2005@gmail.com>
2025-10-04 20:29:53 -0500

    remove unused import

--- src/main/java/net/bunten/enderscape/datagen/EnderscapeRecipeProvider.java
+++ src/main/java/net/bunten/enderscape/datagen/EnderscapeRecipeProvider.java
@@ -17,7 +17,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.equipment.trim.TrimPattern;
 import net.minecraft.world.level.ItemLike;
