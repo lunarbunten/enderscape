c01b70f52ee5706dfef9644a32b459241b65c2ee
bunten <bunten2005@gmail.com>
2025-10-05 23:21:25 -0500

    add missing pressure plate loot tables

--- src/main/java/net/bunten/enderscape/datagen/EnderscapeBlockLootProvider.java
+++ src/main/java/net/bunten/enderscape/datagen/EnderscapeBlockLootProvider.java
@@ -278,6 +278,7 @@ public class EnderscapeBlockLootProvider extends FabricBlockLootTableProvider {
         dropSelf(POLISHED_END_STONE_SLAB);
         dropSelf(POLISHED_END_STONE_WALL);
         dropSelf(POLISHED_END_STONE_BUTTON);
+        dropSelf(POLISHED_END_STONE_PRESSURE_PLATE);
         dropSelf(CHISELED_END_STONE);
 
         dropSelf(CHISELED_PURPUR);
@@ -292,6 +293,7 @@ public class EnderscapeBlockLootProvider extends FabricBlockLootTableProvider {
         dropSelf(POLISHED_MIRESTONE_SLAB);
         dropSelf(POLISHED_MIRESTONE_WALL);
         dropSelf(POLISHED_MIRESTONE_BUTTON);
+        dropSelf(POLISHED_MIRESTONE_PRESSURE_PLATE);
         dropSelf(MIRESTONE_BRICKS);
         dropSelf(MIRESTONE_BRICK_STAIRS);
         dropSelf(MIRESTONE_BRICK_SLAB);
@@ -307,6 +309,7 @@ public class EnderscapeBlockLootProvider extends FabricBlockLootTableProvider {
         dropSelf(POLISHED_VERADITE_SLAB);
         dropSelf(POLISHED_VERADITE_WALL);
         dropSelf(POLISHED_VERADITE_BUTTON);
+        dropSelf(POLISHED_VERADITE_PRESSURE_PLATE);
         dropSelf(VERADITE_BRICKS);
         dropSelf(VERADITE_BRICK_STAIRS);
         dropSelf(VERADITE_BRICK_SLAB);
@@ -322,6 +325,7 @@ public class EnderscapeBlockLootProvider extends FabricBlockLootTableProvider {
         dropSelf(POLISHED_KURODITE_SLAB);
         dropSelf(POLISHED_KURODITE_WALL);
         dropSelf(POLISHED_KURODITE_BUTTON);
+        dropSelf(POLISHED_KURODITE_PRESSURE_PLATE);
         dropSelf(KURODITE_BRICKS);
         dropSelf(KURODITE_BRICK_STAIRS);
         dropSelf(KURODITE_BRICK_SLAB);
