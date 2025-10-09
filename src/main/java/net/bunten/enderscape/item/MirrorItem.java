b9ff8ca747d064e18f8bf14f0698e770b32a94fb
bunten <bunten2005@gmail.com>
2025-10-09 03:31:34 -0500

    fix "Unsafe to teleport" Mirror bug when gliding

--- src/main/java/net/bunten/enderscape/item/MirrorItem.java
+++ src/main/java/net/bunten/enderscape/item/MirrorItem.java
@@ -26,10 +26,7 @@ import net.minecraft.stats.Stats;
 import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Relative;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -145,11 +142,12 @@ public class MirrorItem extends NebuliteToolItem {
     public static Optional<Vec3> getTeleportPosition(MirrorContext context) {
         ServerLevel level = context.linkedLevel();
         LivingEntity user = context.user();
+        EntityDimensions dimensions = user.getDimensions(Pose.STANDING);
+        
+        Vec3 offsetPos = context.linkedPos().above().getBottomCenter().add(0.0, dimensions.height() / 2.0, 0.0);
 
-        Vec3 offsetPos = context.linkedPos().above().getBottomCenter().add(0.0, user.getBbHeight() / 2.0, 0.0);
-
-        VoxelShape shape = Shapes.create(AABB.ofSize(offsetPos, user.getBbWidth() + 1, user.getBbHeight() + 1, user.getBbWidth() + 1).inflate(1.0E-6));
-        Optional<Vec3> freePos = level.findFreePosition(user, shape, offsetPos, user.getBbWidth(), user.getBbHeight(), user.getBbWidth());
+        VoxelShape shape = Shapes.create(AABB.ofSize(offsetPos, dimensions.width() + 1, dimensions.height() + 1, dimensions.width() + 1).inflate(1.0E-6));
+        Optional<Vec3> freePos = level.findFreePosition(user, shape, offsetPos, dimensions.width(), dimensions.height(), dimensions.width());
 
         if (freePos.isPresent()) {
             Vec3 pos = freePos.get();
