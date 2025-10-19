31a3036473ed74b6d77f14bd4dae4f10c407adc4
bunten <bunten2005@gmail.com>
2025-10-19 11:22:51 -0500

    Make Enderman stare sound instance max of 1

--- src/main/java/net/bunten/enderscape/client/EnderscapeClient.java
+++ src/main/java/net/bunten/enderscape/client/EnderscapeClient.java
@@ -2,6 +2,7 @@ package net.bunten.enderscape.client;
 
 import com.google.common.reflect.Reflection;
 import net.bunten.enderscape.client.block.MagniaSproutRenderer;
+import net.bunten.enderscape.client.sound.EndermanStareSoundInstance;
 import net.bunten.enderscape.client.sound.EndermanStaticSoundInstance;
 import net.bunten.enderscape.client.hud.HudElement;
 import net.bunten.enderscape.client.item.NebuliteToolTooltip;
@@ -33,6 +34,7 @@ public class EnderscapeClient implements ClientModInitializer {
 
     public static int postMirrorUseTicks;
 
+    @Nullable public static EndermanStareSoundInstance stareSoundInstance = null;
     @Nullable public static EndermanStaticSoundInstance staticSoundInstance = null;
 
     public static void register(HudElement element) {
