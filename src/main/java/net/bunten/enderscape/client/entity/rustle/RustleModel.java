ce7230f7c5214b6caa149df30c7c54d93d246abf
bunten <bunten2005@gmail.com>
2025-10-20 13:40:52 -0500

    Update rustle model and textures

--- src/main/java/net/bunten/enderscape/client/entity/rustle/RustleModel.java
+++ src/main/java/net/bunten/enderscape/client/entity/rustle/RustleModel.java
@@ -14,6 +14,7 @@ public class RustleModel extends EntityModel<RustleRenderState> {
 	private final ModelPart crossSpines;
 	private final ModelPart middleSpines;
 	private final ModelPart backSpines;
+	private final ModelPart tail;
 	private final ModelPart head;
 	private final ModelPart rightAntenna;
 	private final ModelPart leftAntenna;
@@ -26,6 +27,7 @@ public class RustleModel extends EntityModel<RustleRenderState> {
 		crossSpines = body.getChild("crossSpines");
 		middleSpines = body.getChild("middleSpines");
 		backSpines = body.getChild("backSpines");
+		tail = body.getChild("tail");
 		head = root.getChild("head");
 		rightAntenna = head.getChild("rightAntenna");
 		leftAntenna = head.getChild("leftAntenna");
@@ -36,27 +38,28 @@ public class RustleModel extends EntityModel<RustleRenderState> {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -2.5F, -5.0F, 8.0F, 5.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 21.5F, 2.0F));
-		PartDefinition crossSpines = body.addOrReplaceChild("crossSpines", CubeListBuilder.create().texOffs(0, 27).addBox(-7.0F, -2.5F, 0.0F, 14.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -5.0F, 0.0F, 0.0F, 0.7854F, 0.0F));
+		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -5.0F, -3.0F, 8.0F, 5.0F, 10.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition crossSpines = body.addOrReplaceChild("crossSpines", CubeListBuilder.create().texOffs(0, 27).addBox(-7.0F, -5.0F, 0.25F, 14.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -5.0F, 1.75F));
 
-		crossSpines.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(0, 27).addBox(-7.0F, -1.0F, 0.0F, 14.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -1.5F, 0.0F, 0.0F, 1.5708F, 0.0F));
-		body.addOrReplaceChild("middleSpines", CubeListBuilder.create().texOffs(0, 20).addBox(-7.0F, -4.0F, 0.0F, 14.0F, 7.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -0.5F, -2.0F));
-		body.addOrReplaceChild("backSpines", CubeListBuilder.create().texOffs(0, 20).addBox(-7.0F, -4.0F, 0.0F, 14.0F, 7.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -0.5F, 3.0F));
+		crossSpines.addOrReplaceChild("cube_r1", CubeListBuilder.create().texOffs(1, 27).addBox(-6.0F, -1.0F, 0.0F, 13.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -4.0F, 0.25F, 0.0F, 1.5708F, 0.0F));
+		body.addOrReplaceChild("middleSpines", CubeListBuilder.create().texOffs(0, 20).addBox(-7.0F, -4.5F, 0.0F, 14.0F, 7.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.5F, 0.0F));
+		body.addOrReplaceChild("backSpines", CubeListBuilder.create().texOffs(0, 20).mirror().addBox(-7.0F, -4.5F, 0.0F, 14.0F, 7.0F, 0.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(0.0F, -2.5F, 5.0F));
+		body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(36, 9).addBox(-2.0F, -1.5F, 0.0F, 4.0F, 3.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.5F, 7.0F));
 
-		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(26, 0).addBox(-3.0F, -2.5F, -3.0F, 6.0F, 5.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 21.5F, -4.0F));
+		PartDefinition head = partdefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(26, 0).addBox(-3.0F, -3.0F, -4.0F, 6.0F, 5.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, -3.0F));
 
-		head.addOrReplaceChild("rightAntenna", CubeListBuilder.create().texOffs(46, -7).addBox(0.0F, -7.0F, -5.5F, 0.0F, 7.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, -2.5F, -1.5F));
-		head.addOrReplaceChild("leftAntenna", CubeListBuilder.create().texOffs(46, -7).addBox(0.0F, -7.0F, -5.5F, 0.0F, 7.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, -2.5F, -1.5F));
-		head.addOrReplaceChild("frontSpines", CubeListBuilder.create().texOffs(0, 15).addBox(-7.0F, -2.0F, 0.0F, 14.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -0.5F, -2.0F));
+		head.addOrReplaceChild("rightAntenna", CubeListBuilder.create().texOffs(46, -7).addBox(0.0F, -7.0F, -5.5F, 0.0F, 7.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, -3.0F, -2.5F));
+		head.addOrReplaceChild("leftAntenna", CubeListBuilder.create().texOffs(46, -7).addBox(0.0F, -7.0F, -5.5F, 0.0F, 7.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, -3.0F, -2.5F));
+		head.addOrReplaceChild("frontSpines", CubeListBuilder.create().texOffs(0, 15).addBox(-7.0F, -2.0F, 0.0F, 14.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, -3.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 32);
 	}
 
-    @Override
-    public void setupAnim(RustleRenderState state) {
+	@Override
+	public void setupAnim(RustleRenderState state) {
 		super.setupAnim(state);
 
-        float age = state.ageInTicks;
+		float age = state.ageInTicks;
 		float animPos = state.walkAnimationPos;
 		float animSpeed = state.walkAnimationSpeed;
 
@@ -71,10 +74,11 @@ public class RustleModel extends EntityModel<RustleRenderState> {
 		head.zRot = Mth.sin(age + (animPos / 3) * 0.06F) * animSpeed * 0.5F;
 
 		body.zRot = Mth.sin(age + (animPos / 3) * 0.03F) * animSpeed * 0.25F;
+		tail.zRot = Mth.sin(age + (animPos / 3) * 0.06F) * animSpeed * 0.5F;
 
-        frontSpines.yRot = Mth.sin(age + (animPos / 3) * 0.1F) * animSpeed * 0.8F;
-        middleSpines.yRot = Mth.sin(age + (animPos / 3) * 0.1F + Mth.HALF_PI) * animSpeed * 0.8F;
-        backSpines.yRot = Mth.sin(age + (animPos / 3) * 0.1F + Mth.PI) * animSpeed * 0.8F;
+		frontSpines.yRot = Mth.sin(age + (animPos / 3) * 0.1F) * animSpeed * 0.8F;
+		middleSpines.yRot = Mth.sin(age + (animPos / 3) * 0.1F + Mth.HALF_PI) * animSpeed * 0.8F;
+		backSpines.yRot = Mth.sin(age + (animPos / 3) * 0.1F + Mth.PI) * animSpeed * 0.8F;
 
 		animate(state.sleepingAnimationState, RustleAnimations.SLEEPING, state.ageInTicks, 1.0F);
 
@@ -83,5 +87,5 @@ public class RustleModel extends EntityModel<RustleRenderState> {
 		head.zScale = state.isBaby ? 1.25F : 1;
 
 		crossSpines.visible = !state.isBaby && !state.isSheared;
-    }
+	}
 }
