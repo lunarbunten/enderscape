ccc47fb8febe7efc8af9c8a5ea128b3a53ed4517
xg787 <xaviere949@gmail.com>
2026-02-03 02:20:12 -0500

    fix some mixin stuff

--- src/main/java/net/bunten/enderscape/mixin/ItemMixin.java
+++ src/main/java/net/bunten/enderscape/mixin/ItemMixin.java
@@ -1,18 +1,26 @@
 package net.bunten.enderscape.mixin;
 
 import net.bunten.enderscape.item.FueledTool;
+import net.bunten.enderscape.item.LodestoneTeleporter;
 import net.bunten.enderscape.registry.EnderscapeAttributes;
+import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
 import org.apache.commons.lang3.math.Fraction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(Item.class)
 public class ItemMixin {
@@ -35,5 +43,11 @@ public class ItemMixin {
             Fraction fraction = Fraction.getFraction(FueledTool.currentFuel(stack), FueledTool.maxFuel(stack));
             info.setReturnValue(Mth.clamp(Mth.mulAndTruncate(fraction, 13), 0, 13));
         }
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Inject(method = "appendHoverText", at = @At("HEAD"))
+    public void Enderscape$getName(ItemStack stack, Item.TooltipContext context, List<Component> list, TooltipFlag flag, CallbackInfo ci) {
+        if (LodestoneTeleporter.is(stack)) LodestoneTeleporter.appendHoverText(stack, list);
     }
 }
